03b6563865d7460a543f0b6c230394e1288ba9ef
KatyaXian <123699414+KatyaXian@users.noreply.github.com>
2023-01-26 14:08:41 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,49 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let titlt = document.querySelector(".title");
+let StoryPart1 = document.querySelector(".story-opening");
+let Buttonts = document.querySelector(".buttons");
+let StoryPart2Easy = document.querySelector(".option-one-screen");
+let StoryPart3Hard = document.querySelector(".option-two-screen");
+let StoryPart2end = document.querySelector(".easy-end");
+let StoryPart3end = document.querySelector(".hard-end");
+let Clickeasy = document.querySelector(".option-one");
+let Clickhard = document.querySelector(".option-two");
+let Clickeasyend = document.querySelector(".esayend");
+let Clickhardend = document.querySelector(".hardend");
+
+
+
+
+// When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+//
+Clickeasy.onclick=function(){
+StoryPart2Easy.style.display="block";
+
+};
+//
+
+//
+Clickhard.onclick=function(){
+StoryPart3Hard.style.display="block";
+
+};
+
+//
+
+//
+ Clickeasyend.onmouseover=function(){
+StoryPart2end.style.display="block";
+
+};
+
+//
+
+//
+
+Clickhardend.onmouseover=function(){
+StoryPart3end.style.display="block";
+
+};
+//
+
+
